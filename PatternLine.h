864ba1477862f3fc27130be3dcbea4155351c3bf
Muditha Kodithuwakku <s3813354@student.rmit.edu.au>
2020-05-04 03:17:05 +1000

    patternLine incomplete as i need some other classes

--- PatternLine.h
+++ PatternLine.h
@@ -1,14 +1,14 @@
 #ifndef PATTERNLINE_H
 #define PATTERNLINE_H
 
+#include "Tile.h"
+
 class PatternLine {
 public:
     PatternLine();
     ~PatternLine();
     
-    void addTile();
-    void removeTile();
-    int getNegativePoints();
+    void addTiles(Tile* tile); //void addTiles(TileFactory tileFactory,int tileFactoryNumber, TileColour colour);
     
 private:
     
