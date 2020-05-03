f96294ae9ea0c05b85cb980e8db57457e5a2ae01
Han Chien Leow <s3778722@student.rmit.edu.au>
2020-05-04 02:53:40 +1000

    Added TileCode.h

---
+++ TileCode.h
@@ -0,0 +1,14 @@
+#ifndef TILECODE_H
+#define TILECODE_H
+
+typedef char Colour;
+
+#define RED             'R'
+#define YELLOW          'Y'
+#define DARK_BLUE       'B'
+#define LIGHT_BLUE      'L'
+#define BLACK           'U'
+#define FIRST_PLAYER    'F'
+#define NO_TILE         '.'
+
+#endif  //TILECODE_H
