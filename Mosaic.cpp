02437fab4b5a106c7a273e33e5a8c5e855800a63
Han Chien Leow <s3778722@student.rmit.edu.au>
2020-05-19 15:44:09 +1000

    removed some of my redundant commented code

--- Mosaic.cpp
+++ Mosaic.cpp
@@ -1,13 +1,6 @@
 #include "Mosaic.h"
 
 Mosaic::Mosaic(){
-    /*
-    for(int j=0; j<5; j++){
-        this->mosaicArray[j] = new Tile*[5];
-        for(int i=0;i<5; i++){
-            this->mosaicArray[j][i] = new Tile(); 
-        }
-    }*/
     initMosaicPattern();
     //displayMosaic();
 }
@@ -47,31 +40,7 @@ void Mosaic::displayMosaic(){
     }
 }
 
-//sample idea
 void Mosaic::initMosaicPattern(){
-    /*
-    for(int i = 0; i < 5; i++){
-        this->mosaicArray[i][i] = new Tile('b');
-        this->mosaicArray[i][i+1] = new Tile('y');
-        this->mosaicArray[i][i+2] = new Tile('r');
-        this->mosaicArray[i][i+3] = new Tile('u');
-        this->mosaicArray[i][i+4] = new Tile('l');
-        this->mosaicArray[i][i-1] = new Tile('l');
-        this->mosaicArray[i][i-2] = new Tile('u');
-        this->mosaicArray[i][i-3] = new Tile('r');
-        this->mosaicArray[i][i-4] = new Tile('y');
-    }
-    */
-   /*
-    std::string pattern = "byrullbyruulbyrrulbyyrulb";
-    int counter = 0;
-    for (int i = 0; i < 5; i++){
-        for(int j = 0; j<5; j++){
-            this->mosaicArray[i][j] = new Tile(pattern[counter]);
-            counter++;
-        }
-    }*/
-
     //Tested and it is working
     std::string pattern = "byrullbyruulbyrrulbyyrulb";
     int counter = 0;
@@ -108,33 +77,3 @@ bool Mosaic::hasFullRow(){
     }
     return full;
 }
-/**
- * If patternlinefullrow == true
- * forloop
- *  If mosaicArray[i][j].getcolour() == lower(c)
- *     mosaicArray[i][j].setcolour(c)   
-**/
-/*
-bool Mosaic::hasFullRow()
-{
-    bool full;
-    for (int row = 0; row < 5; row++)
-    {
-        full = true;
-        bool next = false;
-        for (int column = 0; column < 5 && next == false; column++)
-        {
-            if (!mosaicArray[row][column])
-            {
-                //skip this row if not full
-                full = false;
-                next = true;
-                }
-            }
-        if (full){
-            return true;
-        }
-    }
-    return false;
-}
-*/
